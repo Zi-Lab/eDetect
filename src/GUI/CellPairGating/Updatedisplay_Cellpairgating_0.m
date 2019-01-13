1c3030a4855dcae8cbad6330ac2dffab54109330
Zhike Zi <zhike.zi@gmail.com>
2019-01-13 19:14:40 +0100

    updated

--- src/GUI/CellPairGating/Updatedisplay_Cellpairgating_0.m
+++ src/GUI/CellPairGating/Updatedisplay_Cellpairgating_0.m
@@ -1,16 +1,22 @@
 function param = Updatedisplay_Cellpairgating_0(param)
-cla(param.hNucleiCellpairGating.axes1,'reset');
-l = length(param.hNucleiCellpairGating.axes1.Children);
-for i = l:(-1):1
-    if isa(param.hNucleiCellpairGating.axes1.Children(i),'matlab.graphics.chart.primitive.Line')
-        delete(param.hNucleiCellpairGating.axes1.Children(i));
+if isfield(param,'hNucleiCellpairGating')
+    if isfield(param.hNucleiCellpairGating,'points_s')
+        if isvalid(param.hNucleiCellpairGating.points_s)
+            delete(param.hNucleiCellpairGating.points_s);
+        end
+    end
+    if isfield(param.hNucleiCellpairGating,'points_us')
+        if isvalid(param.hNucleiCellpairGating.points_us)
+            delete(param.hNucleiCellpairGating.points_us);
+        end
     end
 end
 id_s  =   param.tmp.cellpair_gating_selected;
 id_us = ~ param.tmp.cellpair_gating_selected;
-plot(param.tmp.pc_cellpairgating(id_s,1) ,param.tmp.pc_cellpairgating(id_s,2) ,'r.','Parent',param.hNucleiCellpairGating.axes1,'HitTest','off'),hold on;
-plot(param.tmp.pc_cellpairgating(id_us,1),param.tmp.pc_cellpairgating(id_us,2),'b.','Parent',param.hNucleiCellpairGating.axes1,'HitTest','off'),hold on;
+hold( param.hNucleiCellpairGating.axes1, 'on' );
+param.hNucleiCellpairGating.points_s  = plot(param.tmp.pc_cellpairgating(id_s,1) ,param.tmp.pc_cellpairgating(id_s,2) ,'r.','Parent',param.hNucleiCellpairGating.axes1,'HitTest','off');
+param.hNucleiCellpairGating.points_us = plot(param.tmp.pc_cellpairgating(id_us,1),param.tmp.pc_cellpairgating(id_us,2),'b.','Parent',param.hNucleiCellpairGating.axes1,'HitTest','off');
 id_s  = [];
 id_us = [];
-set(param.hNucleiCellpairGating.axes1,'ButtonDownFcn',@CallbackMouseClickOnCellpairGating,'HitTest','on'),hold on;
+set(param.hNucleiCellpairGating.axes1,'ButtonDownFcn',@CallbackMouseClickOnCellpairGating,'HitTest','on');
 end
--- src/GUI/SegmentationGating/Updatedisplay_Segmentationgating_0.m
+++ src/GUI/SegmentationGating/Updatedisplay_Segmentationgating_0.m
@@ -1,11 +1,4 @@
 function param = Updatedisplay_Segmentationgating_0(param)
-%l = length(param.hNucleiSegmentationGating.axes1.Children);
-%for i = l:(-1):1
-%    if isa(param.hNucleiSegmentationGating.axes1.Children(i),'matlab.graphics.chart.primitive.Line')
-%        delete(param.hNucleiSegmentationGating.axes1.Children(i));
-%    end
-%end
-%cla(param.hNucleiSegmentationGating.axes1,'reset');
 if isfield(param,'hNucleiSegmentationGating')
     if isfield(param.hNucleiSegmentationGating,'points_s_d')
         if isvalid(param.hNucleiSegmentationGating.points_s_d)
@@ -34,24 +27,12 @@ id_s_ud  =  param.tmp.segmentation_gating_selected & ~param.tmp.gating_deleted;
 id_us_d  = ~param.tmp.segmentation_gating_selected &  param.tmp.gating_deleted;
 id_us_ud = ~param.tmp.segmentation_gating_selected & ~param.tmp.gating_deleted;
 hold( param.hNucleiSegmentationGating.axes1, 'on' );
-%{
-if param.set.deleted_objects_display == 1
-    param.hNucleiSegmentationGating.points_s_d = plot(param.tmp.pc_segmentationgating(id_s_d,1),   param.tmp.pc_segmentationgating(id_s_d,2),   'ro','Parent',param.hNucleiSegmentationGating.axes1,'HitTest','off');%,hold on;
-elseif param.set.deleted_objects_display == 2
-    param.hNucleiSegmentationGating.points_s_d = plot(param.tmp.pc_segmentationgating(id_s_d,1),   param.tmp.pc_segmentationgating(id_s_d,2),   'r.','Parent',param.hNucleiSegmentationGating.axes1,'HitTest','off');%,hold on;
-end
-if param.set.deleted_objects_display == 1
-    param.hNucleiSegmentationGating.points_us_d = plot(param.tmp.pc_segmentationgating(id_us_d,1),  param.tmp.pc_segmentationgating(id_us_d,2),  'bo','Parent',param.hNucleiSegmentationGating.axes1,'HitTest','off');%,hold on;
-elseif param.set.deleted_objects_display == 2
-    param.hNucleiSegmentationGating.points_us_d = plot(param.tmp.pc_segmentationgating(id_us_d,1),  param.tmp.pc_segmentationgating(id_us_d,2),  'g.','Parent',param.hNucleiSegmentationGating.axes1,'HitTest','off');%,hold on;
-end
-%}
 if strcmp( get(param.hNucleiSegmentationGating.toggletool_DisplayDeleted,'State') , 'on')
-    param.hNucleiSegmentationGating.points_s_d = plot(param.tmp.pc_segmentationgating(id_s_d,1),   param.tmp.pc_segmentationgating(id_s_d,2),   'r.','Parent',param.hNucleiSegmentationGating.axes1,'HitTest','off');%,hold on;
-    param.hNucleiSegmentationGating.points_us_d = plot(param.tmp.pc_segmentationgating(id_us_d,1),  param.tmp.pc_segmentationgating(id_us_d,2),  'g.','Parent',param.hNucleiSegmentationGating.axes1,'HitTest','off');%,hold on;
+    param.hNucleiSegmentationGating.points_s_d = plot(param.tmp.pc_segmentationgating(id_s_d,1),   param.tmp.pc_segmentationgating(id_s_d,2),   'r.','Parent',param.hNucleiSegmentationGating.axes1,'HitTest','off');
+    param.hNucleiSegmentationGating.points_us_d = plot(param.tmp.pc_segmentationgating(id_us_d,1),  param.tmp.pc_segmentationgating(id_us_d,2),  'g.','Parent',param.hNucleiSegmentationGating.axes1,'HitTest','off');
 end
-param.hNucleiSegmentationGating.points_us_ud = plot(param.tmp.pc_segmentationgating(id_us_ud,1), param.tmp.pc_segmentationgating(id_us_ud,2), 'b.','Parent',param.hNucleiSegmentationGating.axes1,'HitTest','off');%,hold on;
-param.hNucleiSegmentationGating.points_s_ud = plot(param.tmp.pc_segmentationgating(id_s_ud,1),  param.tmp.pc_segmentationgating(id_s_ud,2),  'r.','Parent',param.hNucleiSegmentationGating.axes1,'HitTest','off');%,hold on;
+param.hNucleiSegmentationGating.points_us_ud = plot(param.tmp.pc_segmentationgating(id_us_ud,1), param.tmp.pc_segmentationgating(id_us_ud,2), 'b.','Parent',param.hNucleiSegmentationGating.axes1,'HitTest','off');
+param.hNucleiSegmentationGating.points_s_ud = plot(param.tmp.pc_segmentationgating(id_s_ud,1),  param.tmp.pc_segmentationgating(id_s_ud,2),  'r.','Parent',param.hNucleiSegmentationGating.axes1,'HitTest','off');
 id_s_d = [];
 id_s_ud = [];
 id_us_d = [];
