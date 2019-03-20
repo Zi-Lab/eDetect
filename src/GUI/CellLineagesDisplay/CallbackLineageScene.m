700290b1544544d3bd9335557ba7e2ede4f6d62b
Zhike Zi <zhike.zi@gmail.com>
2019-03-20 14:56:06 +0100

    updated

--- src/GUI/CellLineagesDisplay/CallbackLineageScene.m
+++ src/GUI/CellLineagesDisplay/CallbackLineageScene.m
@@ -4,6 +4,8 @@ if param.tmp.n_scene ~= 1
     CurrentLineageScene = round((get(param.hLineage.SliderFrame1,'Value')));
     set(param.hMain.SliderFrame1, 'value', CurrentLineageScene);
 end
+param.hLineage.Edit_cmin.String = '';
+param.hLineage.Edit_cmax.String = '';
 param = Updatedisplay_Image_1(param);
 param = Updatedisplay_Heatmap_3(param);
 InformAllInterfaces(param);
