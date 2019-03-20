a9f855be7e2ac8db983c47b716c28d916c6abc0b
Zhike Zi <zhike.zi@gmail.com>
2019-03-20 15:16:54 +0100

    updated

--- src/GUI/CellLineagesDisplay/CallbackLineageScene.m
+++ src/GUI/CellLineagesDisplay/CallbackLineageScene.m
@@ -4,6 +4,9 @@ if param.tmp.n_scene ~= 1
     CurrentLineageScene = round((get(param.hLineage.SliderFrame1,'Value')));
     set(param.hMain.SliderFrame1, 'value', CurrentLineageScene);
 end
+param.tmp.manual_list_selected_objects = [];
+param.tmp.manual_list_selected_child = [];
+param.tmp.manual_list_selected_parent = [];
 param.hLineage.Edit_cmin.String = '';
 param.hLineage.Edit_cmax.String = '';
 param = Updatedisplay_Image_1(param);
